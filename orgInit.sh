e50b22f39aae92a2873be14f4f5b2faf2c79f346
Chad Evans <chad.evans@salesforce.com>
2019-07-12 09:04:47 -0400

    Scramble User Info use case

--- orgInit.sh
+++ orgInit.sh
@@ -1,5 +1,8 @@
 sfdx force:org:create -f config/project-scratch-def.json -d 30 -s -w 60
 
+# Install the package
+sfdx force:package:install --package 04t1t000003HRGaAAO -w 30
+
 sfdx force:source:push
 sfdx force:user:permset:assign -n ExtId_Admin
 
